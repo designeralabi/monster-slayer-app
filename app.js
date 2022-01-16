2289d629bf9939a0212e324a4256af73eb563e16
designeralabi <bdlkrmalabi@gmail.com>
2022-01-16 22:07:24 +0100

    health bar logic on attack bar fixed

--- app.js
+++ app.js
@@ -7,8 +7,25 @@ const app = Vue.createApp({
     return {
       playerHealth: 100,
       monsterHealth: 100,
-      counterRound: 0
+      counterRound: 0,
+      winner: null
     };
+  },
+  watch: {
+    playerHealth() {
+      if (this.playerHealth <=0 && this.monsterHealth <=0) {
+        this.winner = 'draw';
+      }else if(this.playerHealth <= 0){
+        this.winner = 'monster';
+      }
+    },
+    monsterHealth(){
+      if (this.monsterHealth <=0 && this.playerHealth <=0){
+        this.winner = 'draw';
+      }else if(this.monsterHealth <= 0){
+         this.winner = 'player';
+      }
+    }
   },
   computed: {
     playerHealthBar() {
@@ -17,22 +34,35 @@ const app = Vue.createApp({
     monsterHealthBar() {
       return { width: this.monsterHealth + "%" };
     },
+    useSpecialAttack() {
+      return this.counterRound % 3 !== 0;
+    },
   },
   methods: {
     attackMonster() {
-        this.counterRound++;
+      this.counterRound++;
       let attackStrength = getRandomValue(5, 12);
       this.monsterHealth -= attackStrength;
       this.attackPlayer();
     },
     attackPlayer() {
-      let monsterAttackStrength = getRandomValue(8, 5);
+      let monsterAttackStrength = getRandomValue(8, 15);
       this.playerHealth -= monsterAttackStrength;
     },
     specialAttackMonster() {
-        this.counterRound++;
+      this.counterRound++;
       let attackStrength = getRandomValue(10, 25);
       this.monsterHealth -= attackStrength;
+      this.attackPlayer();
+    },
+    healPlayer() {
+      this.counterRound++;
+      const playerhealthrestore = getRandomValue(8, 20);
+      if (this.playerHealth + playerhealthrestore > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += playerhealthrestore;
+      }
       this.attackPlayer();
     },
   },
